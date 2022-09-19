50fd32357f2b14f6ee68a6fad29f837bb2ea75e6
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 00:46:22 +0200

    get Remote JSON from remote server and show into the app

--- VideoCollection/ViewModels/VideoModels.swift
+++ VideoCollection/ViewModels/VideoModels.swift
@@ -16,7 +16,9 @@ class VideoModels: ObservableObject {
     
     init() {
         
-        getJsonData()
+//        getJsonData()
+        
+        getRemoteData()
     }
     
     func getJsonData() {
@@ -52,11 +54,57 @@ class VideoModels: ObservableObject {
         }
     }
     
-    func retrieveVideoUrl() {
+    func getRemoteData() {
         
-        for i in 0..<model.count {
+        // https://mukulsustcse.github.io/VideoCollectionApp_data/Data.json
+        
+        let urlString = "https://mukulsustcse.github.io/VideoCollectionApp_data/Data.json"
+        
+        let url = URL(string: urlString)
+        
+        guard url != nil else {
             
-            videoUrl = model[i].url
+            // cannot create url
+            return
         }
+        
+        // create a URL Request object
+        let request = URLRequest(url: url!)
+        
+        // get the session and kick off the task
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            
+            // check if there is an error
+            guard error == nil else {
+                
+                // there was an error
+                return
+            }
+            
+            do {
+                // create json decoder
+                let decoder = JSONDecoder()
+                
+                // decode
+                let modules = try decoder.decode([Models].self, from: data!)
+                
+                DispatchQueue.main.async {
+                    
+                    // append parsed modules into modules property
+                    self.model = modules
+                }
+            }
+            catch {
+                
+                print(error)
+            }
+        }
+        
+        // kick off data task
+        dataTask.resume()
     }
+    
 }
