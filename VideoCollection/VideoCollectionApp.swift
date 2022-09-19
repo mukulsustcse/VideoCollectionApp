16390cfcf3114876ef4bf68e14fef1f684dca953
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-19 22:06:32 +0200

    Initial Commit

---
+++ VideoCollection/VideoCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCollectionApp.swift
+//  VideoCollection
+//
+//  Created by M. A. Alim Mukul on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct VideoCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
